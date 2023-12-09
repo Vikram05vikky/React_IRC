7fd039abbacea2a5b7fc4ad2c91a5b9ec3d0d565
Vikram05vikky <vikram05sar@gmail.com>
2023-12-09 16:28:35 +0530

    Day 2

---
+++ Day2/sampleapp/src/App.js
@@ -0,0 +1,32 @@
+import './Assets/css/style.css'
+import Nav from './Components/Nav'
+function App()
+{
+    return(
+        <div>
+            <Nav/>
+           <form class="container">
+        <h2>Registration form</h2>
+        <input type="text" name="" id="name" className="name" placeholder="Name" required/>
+
+        <input type="tel" name="" id="phone" className="name"  placeholder="Phone No" required/>
+
+        <input type="number" name="" id="age" className="name"  placeholder="Age" required/>
+
+        <input type="email" name="" id="email" className="name"  placeholder="Email" required/>
+
+        <input type="password" name="" id="pass1" className="name"  placeholder="Password" required/>
+        <input type="password" name="" id="pass2" className="name"  placeholder="Confirm Password" required/>
+
+        <input type="date" name="" id="dob" className="name"  placeholder="Date of Birth" required/>
+
+        <input type="text" name="" id="Address" className="name"  placeholder="Address" required/>
+
+        <button class="btn" onclick="show()">Register</button>
+    </form>
+        
+    
+        </div>
+    )
+}
+export default App
---
+++ Day2/sampleapp/src/Components/Nav.js
@@ -0,0 +1,13 @@
+import './Assets/css/navbar.css'
+
+function Nav()
+{
+    return(
+        <div>
+        <a href="login.html">Login</a>
+        <a href="form.html">Register</a>
+        </div>
+    )
+}
+
+export default Nav
