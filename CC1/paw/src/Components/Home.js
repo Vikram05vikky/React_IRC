56fe0dee0ab47e09a8b8fd3813e2b1dc6cf7de5a
Vikram05vikky <vikram05sar@gmail.com>
2023-12-18 23:37:24 +0530

    paws

--- CC1/paw/src/Components/Home.js
+++ CC1/paw/src/Components/Home.js
@@ -1,17 +1,27 @@
 import React from "react";
-import "../Assets/Landing.css";
+// import "../Assets/Landing.css";
 import Shine from "./Shine";
 import Sidebar from "./Sidebar";
+import "../Assets/Home.css";
 function Home() {
   return (
-    <>
+    <body className="dv2">
       <div className="abc">
-        <div>
-          <Sidebar />
-          {/* <Shine /> */}
+        <Sidebar />
+        <div className="dv1">
+          <h1
+            style={{
+              fontStyle: "italic",
+              fontSize: "60px",
+              fontFamily: "'Times New Roman', Times, serif",
+            }}
+          >
+            “ You can’t change their past,<br></br> but you can rewrite<br></br>{" "}
+            their future. ”
+          </h1>
         </div>
       </div>
-    </>
+    </body>
   );
 }
 
--- CC1/paw/src/Components/Sidebar.js
+++ CC1/paw/src/Components/Sidebar.js
@@ -6,6 +6,9 @@ import { Cat } from "lucide-react";
 import { Rabbit } from "lucide-react";
 import { PawPrint } from "lucide-react";
 import { Home } from "lucide-react";
+import { LogOut } from "lucide-react";
+import { Link } from "react-router-dom";
+
 function Sidebar() {
   return (
     <div>
@@ -13,7 +16,40 @@ function Sidebar() {
         rel="stylesheet"
         href="path/to/font-awesome/css/font-awesome.min.css"
       ></link>
+      <div class="button-container">
+        <nav className="n1">
+          <i class="paw" style={{ position: "absolute", top: 2, left: 10 }}>
+            <PawPrint size={50} />
+          </i>
+        </nav>
+        <button class="button">
+          <Link to="/Home" style={{ textDecoration: "none", color: "white" }}>
+            Home
+          </Link>
+        </button>
+        <button class="button">
+          <Link to="/About" style={{ textDecoration: "none", color: "white" }}>
+            About
+          </Link>
+        </button>
+        <button class="button">
+          <Link
+            to="/Contact"
+            style={{ textDecoration: "none", color: "white" }}
+          >
+            Contact
+          </Link>
+        </button>
+        <button class="button">
+          <Link to="/Info" style={{ textDecoration: "none", color: "white" }}>
+            Info
+          </Link>
+        </button>
+        {/* <button class="button">About</button>
+        <button class="button">Contact</button>
 
+        <button class="button">Info</button> */}
+      </div>
       <nav className="lnav">
         <a href="#" className="item" data-text="Dog">
           <Dog size={48} />
@@ -31,99 +67,11 @@ function Sidebar() {
           <Rabbit size={48} />
         </a>
         <a href="#" className="item" data-text="Logout">
-          <i class="fas fa-sign-in-alt"></i>
+          <Link to="/Login">
+            <LogOut size={48} />
+          </Link>
         </a>
       </nav>
-      {/* <div class="nav">
-        <div class="nav-header">
-          <div class="nav-title">Paw</div>
-        </div>
-
-        <div class="nav-links">
-          <a target="_blank">Home</a>
-          <a target="_blank">Info</a>
-          <a target="_blank">Contact</a>
-          <a target="_blank">About</a>
-          <a target="_blank">Logout</a>
-        </div> */}
-      {/* </div> */}
-      <div class="button-container">
-        <nav className="n1">
-          <i class="paw">
-            <PawPrint />
-          </i>
-        </nav>
-        <button class="button">
-          {/* <svg
-            // xmlns="http://www.w3.org/2000/svg"
-            width="1em"
-            height="1em"
-            viewBox="0 0 1024 1024"
-            stroke-width="0"
-            fill="currentColor"
-            stroke="currentColor"
-            class="icon"
-          > */}
-          {/* <path d="M946.5 505L560.1 118.8l-25.9-25.9a31.5 31.5 0 0 0-44.4 0L77.5 505a63.9 63.9 0 0 0-18.8 46c.4 35.2 29.7 63.3 64.9 63.3h42.5V940h691.8V614.3h43.4c17.1 0 33.2-6.7 45.3-18.8a63.6 63.6 0 0 0 18.7-45.3c0-17-6.7-33.1-18.8-45.2zM568 868H456V664h112v204zm217.9-325.7V868H632V640c0-22.1-17.9-40-40-40H432c-22.1 0-40 17.9-40 40v228H238.1V542.3h-96l370-369.7 23.1 23.1L882 542.3h-96.1z"></path> */}
-          <h3>Home</h3>
-          {/* </svg> */}
-        </button>
-        <button class="button">
-          {/* <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="1em"
-            height="1em"
-            aria-hidden="true"
-            viewBox="0 0 24 24"
-            stroke-width="2"
-            fill="none"
-            stroke="currentColor"
-            class="icon"
-          > */}
-          {/* <path
-              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-              stroke-linejoin="round"
-              stroke-linecap="round"
-            ></path> */}
-          About
-          {/* </svg> */}
-        </button>
-        <button class="button">
-          {/* <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="1em"
-            height="1em"
-            viewBox="0 0 24 24"
-            stroke-width="0"
-            fill="currentColor"
-            stroke="currentColor"
-            class="icon"
-          >
-            <path d="M12 2.5a5.5 5.5 0 0 1 3.096 10.047 9.005 9.005 0 0 1 5.9 8.181.75.75 0 1 1-1.499.044 7.5 7.5 0 0 0-14.993 0 .75.75 0 0 1-1.5-.045 9.005 9.005 0 0 1 5.9-8.18A5.5 5.5 0 0 1 12 2.5ZM8 8a4 4 0 1 0 8 0 4 4 0 0 0-8 0Z"></path>
-          </svg> */}
-          Contact
-        </button>
-
-        <button class="button">
-          {/* <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="1em"
-            height="1em"
-            stroke-linejoin="round"
-            stroke-linecap="round"
-            viewBox="0 0 24 24"
-            stroke-width="2"
-            fill="none"
-            stroke="currentColor"
-            class="icon"
-          >
-            <circle r="1" cy="21" cx="9"></circle>
-            <circle r="1" cy="21" cx="20"></circle>
-            <path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path>
-          </svg> */}
-          Info
-        </button>
-      </div>
     </div>
   );
 }
